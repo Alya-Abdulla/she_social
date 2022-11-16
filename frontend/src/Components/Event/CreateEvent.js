31bc5bc2c35264de5719bf2ce4b8e4beef179c21
Alya_Abdulla <3lya3bdulla@gmail.com>
2022-11-16 13:29:35 +0300

    try to upload img

--- frontend/src/Components/Event/CreateEvent.js
+++ frontend/src/Components/Event/CreateEvent.js
@@ -2,13 +2,61 @@ import React, {useEffect, useState } from 'react'
 import axios from 'axios'
 import './CreateEvent.css'
 
-
+  // const Upload = () => {
 function CreateEvent() {
-  useEffect(() => {
+      useEffect(() => {
+        
+        event_create_post()
+      }, [])
+      
+      const [fileInputState, setFileInputState] = useState('');
+      const [previewSource, setPreviewSource] = useState('');
+      const [selectedFile, setSelectedFile] = useState();
+      const [successMsg, setSuccessMsg] = useState('');
+      const [errMsg, setErrMsg] = useState('');
+      const handleFileInputChange = (e) => {
+          const file = e.target.files[0];
+          previewFile(file);
+          setSelectedFile(file);
+          setFileInputState(e.target.value);
+      };
     
-    event_create_post()
-    }, [])
+      const previewFile = (file) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onloadend = () => {
+              setPreviewSource(reader.result);
+          };
+          reader.onerror = () => {
+            console.error('AHHHHHHHH!!');
+            setErrMsg('something went wrong!');
+        };
+    };
 
+    const uploadImage = async (base64EncodedImage) => {
+        try {
+            await fetch('/api/upload', {
+                method: 'POST',
+                body: JSON.stringify({ data: base64EncodedImage }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+            setFileInputState('');
+            setPreviewSource('');
+            setSuccessMsg('Image uploaded successfully');
+        } catch (err) {
+            console.error(err);
+            setErrMsg('Something went wrong!');
+        }
+      };
+    
+      const handleSubmitFile = (e) => {
+          e.preventDefault();
+          if (!selectedFile) return;
+          const reader = new FileReader();
+          reader.readAsDataURL(selectedFile);
+          reader.onloadend = () => {
+              uploadImage(reader.result);
+          };}
     const event_create_post = () => {
       const token = localStorage.getItem('token')
       axios.post(`http://localhost:4000/users/events`,
@@ -47,7 +95,26 @@ function CreateEvent() {
   }
   return (
     <div>
-
+ <form onSubmit={handleSubmitFile} className="form">
+                <input
+                    id="fileInput"
+                    type="file"
+                    name="image"
+                    onChange={handleFileInputChange}
+                    value={fileInputState}
+                    className="form-input"
+                />
+                <button className="btn" type="submit">
+                    Submit
+                </button>
+            </form>
+            {previewSource && (
+                <img
+                    src={previewSource}
+                    alt="chosen"
+                    style={{ height: '300px' }}
+                />
+            )}
 <form onSubmit={handleSubmit} className='homeform'>
        <br />
        <label>Create your Event </label> <br></br>
@@ -66,3 +133,10 @@ function CreateEvent() {
 }
 
 export default CreateEvent
+
+
+
+
+
+
+
